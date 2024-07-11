7f72950ef0787c7a06dddeb45f3a5433be785bf5
bishopone <abeselomsolomon106@gmail.com>
2024-07-11 17:40:21 +0300

    cors option

--- index.js
+++ index.js
@@ -51,8 +51,10 @@ app.use(flash());
 
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function (req, file, cb) {
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'uploads'));
+  },
+    filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
